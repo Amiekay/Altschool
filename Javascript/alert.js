4ff2cb5d47af82e36600607d43b91142b9f3a687
Amiekay <amara.kaluagu@gmail.com>
2023-05-31 10:38:44 -0400

    Conditionals

--- Javascript/alert.js
+++ Javascript/alert.js
@@ -1,6 +1,15 @@
-let answer = prompt("What is the “official” name of JavaScript?", "");
+/*let answer = prompt("What is the “official” name of JavaScript?", "");
 if (answer == "ECMAScript") {
   alert("Right!");
 } else {
   alert("You don’t knowECMAScript!");
+}
+*/
+let value = prompt("Type in a number", "");
+if (value > 0) {
+  alert("1");
+} else if (value < 0) {
+  alert("-1");
+} else {
+  alert(0);
 }
