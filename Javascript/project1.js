7ffe500b69359640775fc037b5eeb421cfcbf9cc
Amiekay <amara.kaluagu@gmail.com>
2023-06-08 11:40:55 -0400

    Password confirmation

--- Javascript/project1.js
+++ Javascript/project1.js
@@ -8,5 +8,12 @@ do {
   passWord = prompt("Enter your password", "");
 } while (passWord.length < 6);
 
+let confirmPassWord = prompt("Confirm password", "");
+if (passWord === confirmPassWord) {
+  alert("Confirmed!");
+} else {
+  alert("wrong!");
+}
+
 let userDetails = { userName, passWord };
 console.log(userDetails);
